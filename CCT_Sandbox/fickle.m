68798fdcec73b9bf4f0c69064f0f4db39e8b6366
Erik Knight <eriklknight@gmail.com>
2014-09-17 09:31:53 -0700

    CleanOut
    Removed unused files.

--- CCT_Sandbox/fickle.m
+++
@@ -1,7 +0,0 @@
-function fickle(varargin)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
-fprintf('You are too fickle. \nNow which key would you like?\n');
-end
-
